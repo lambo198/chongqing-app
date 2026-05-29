2e1871b04c4c85f24b7d6547b3d448fbfdb3b0df
lambo198 <sandox79@gmail.com>
2026-05-29 23:32:57 +0200

    Create MainActivity.java

---
+++ .github/workflows/gradle/wrapper/app/app/src/main/app/src/main/java/com/viaje/chongqing/MainActivity.java
@@ -0,0 +1,26 @@
+package com.viaje.chongqing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+    @Override
+    public void onBackPressed() {
+    }
+}
